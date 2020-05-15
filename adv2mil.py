5d18c8f99835856840d32e4c81700e6f21018f13
Hui <jeffrey.x.hui@gmail.com>
2020-05-15 07:42:29 -0700

    down to 997 moves

--- adv2mil.py
+++ adv2mil.py
@@ -2,8 +2,6 @@ from room import Room
 from player import Player
 from world import World
 
-from util import Queue
-
 import random
 from ast import literal_eval
 
@@ -15,8 +13,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+map_file = "maps/test_loop_fork.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -40,65 +38,43 @@ room.get_room_in_direction(direction)
 room.get_coords()
 '''
 
+oppoCards = {
+    'n': 's',
+    'e': 'w',
+    's': 'n',
+    'w': 'e'
+}
+
 traversal_path = []
 visited_rooms = set()
 
 visited_rooms.add(player.current_room)
 
+backtrackCounter = 1
+
 while len(visited_rooms) < len(room_graph):
 
     exits = player.current_room.get_exits()
-    unvisitedRoom = []
+    unvisitedDir = []
     for ex in exits:
         
         # if Room in direction is visited, don't need to visit
         room = player.current_room.get_room_in_direction(ex)
         if room not in visited_rooms:
-            unvisitedRoom.append((ex, room))
+            unvisitedDir.append((ex, room))
     
-    if len(unvisitedRoom) > 0: # calculate immediacy instead when working
-
-        player.travel(unvisitedRoom[0][0]) # [first possible dir][ex]
-        traversal_path.append(unvisitedRoom[0][0])
+    if len(unvisitedDir) >= 1:
+        player.travel(unvisitedDir[0][0]) # [first possible dir][ex]
+        traversal_path.append(unvisitedDir[0][0])
 
         visited_rooms.add(player.current_room)
+        backtrackCounter = 1
 
-    elif len(unvisitedRoom) < 1: # no rooms I need to visit in vicinity, bft backtrack
-        bfQ = Queue()
-        c_room = player.current_room
-        bfQ.enqueue((player.current_room, [])) # room / returnPath
-
-        travelMap = []
-# c_room[0] == object, c_room[1] == directions
-        brk = False
-
-        qVisited = set()
-        qVisited.add(c_room)
-        while brk == False: 
-            c_room, directions = bfQ.dequeue()
-            c_exits = c_room.get_exits()
-            
-            for exs in c_exits:
-                dirRoom = c_room.get_room_in_direction(exs)
-
-                if dirRoom not in visited_rooms: # I should be checking neighbors, not the room itself
-                    travelMap = directions + [exs]
-                    brk = True
-                    break
-
-                if dirRoom not in qVisited:
-                    bfQ.enqueue((dirRoom, directions + [exs]))
-                    qVisited.add(c_room)
-
-
-        for d in travelMap:
-            player.travel(d) # [first possible dir][ex]
-            traversal_path.append(d)
-
-            visited_rooms.add(player.current_room)
-
-
-
+    elif len(unvisitedDir) < 1:
+        player.travel(oppoCards[traversal_path[-1 * backtrackCounter]])
+        traversal_path.append(oppoCards[traversal_path[-1 * backtrackCounter]])
+        backtrackCounter += 2
+print(traversal_path)
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
